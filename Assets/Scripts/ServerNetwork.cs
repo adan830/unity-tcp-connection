a16bd40c7a257575f74848b832143e8b839aefb3
Kirill Rybin <kirya@kirillrybin.com>
2013-09-09 00:52:25 +0400

    scenes and scripts

---
+++ Assets/Scripts/ServerNetwork.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class ServerNetwork : MonoBehaviour {
+    private int port = 25000;
+    private int playerCount = 0;
+    private string _messageLog = "";
+ 
+    public void Awake() {
+        if (Network.peerType == NetworkPeerType.Disconnected)
+            Network.InitializeServer(10, port, false);
+    }
+ 
+    public void Update() {
+ 
+    }
+    public void OnGUI() {
+        if (Network.peerType == NetworkPeerType.Server) {
+            GUI.Label(new Rect(100, 100, 150, 25), "Server");
+            GUI.Label(new Rect(100, 125, 150, 25), "Clients attached: " + Network.connections.Length);
+ 
+            if (GUI.Button(new Rect(100, 150, 150, 25), "Quit server")) {
+                Network.Disconnect();
+                Application.Quit();
+            }
+            if (GUI.Button(new Rect(100, 175, 150, 25), "Send hi to client"))
+                SendInfoToClient();
+        }
+        GUI.TextArea(new Rect(275, 100, 300, 300), _messageLog);
+    }
+ 
+    void OnPlayerConnected(NetworkPlayer player) {
+        AskClientForInfo(player);
+    }
+ 
+    void AskClientForInfo(NetworkPlayer player) {
+        networkView.RPC("SetPlayerInfo", player, player);
+    }
+ 
+    [RPC]
+    void ReceiveInfoFromClient(string someInfo) {
+        _messageLog += someInfo + "\n";
+    }
+ 
+    string someInfo = "Server: hello client";
+    [RPC]
+    void SendInfoToClient() {
+        networkView.RPC("ReceiveInfoFromServer", RPCMode.Others, someInfo);
+    }
+ 
+    // fix RPC errors
+    [RPC]
+    void SendInfoToServer() { }
+    [RPC]
+    void SetPlayerInfo(NetworkPlayer player) { }
+    [RPC]
+    void ReceiveInfoFromServer(string someInfo) { }
+}
